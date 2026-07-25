4a0172698f6de2b0d870601322134891ff051c24
Ahyan <ahyannubaid1003@gmail.com>
2026-07-25 21:29:55 +0700

    sederhanakan service worker: selalu fetch dari network

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,11 @@
-const CACHE_NAME = "expense-tracker-v2";
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/app.js",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
-];
-
 self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
